9178d0ce8f43e970a0515b3ec30fbc62db58208a
yukiiimak <yukimak821@gmail.com>
2021-11-28 01:41:21 -0500

    played around with motionpath

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -8,8 +8,11 @@ const mainTL = gsap.timeline();
 
 function TL(){
     const tl =gsap.timeline();
-    tl.to("#box", {duration:2, y:"+=710", rotate:-40, ease:"none"}); 
-    tl.to("#box", {duration:1, rotate:0, ease:"none"});
+    tl.to("#box", {duration:2, y:"+=710", rotate:-40, ease:"none"}, "fall-tilt"); 
+    tl.to("#box", {duration:1.5, rotate:0, ease:"none"}, "upright");
+    tl.to("#cover", {duration:2, rotate:-35, delay:.3, ease:"none"}, "fall-tilt");
+    tl.to("#cover", {duration:1.5, rotate:0, ease:"none"}, "upright"); 
+    // tl.to("#piping-bag", {duration:15, motionPath:{path:"#motion-path", align:"#motion-path"}}); 
     return tl;
 }
 
