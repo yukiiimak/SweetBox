6a729dca1c76b1744a4fecf21fab50fcc520cbef
yukiiimak <yukimak821@gmail.com>
2021-11-28 01:16:23 -0500

    box fall into screen and slanted

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -2,13 +2,13 @@ import { gsap } from "gsap";
 import { MotionPathPlugin } from "gsap/MotionPathPlugin";
 
 gsap.registerPlugin(MotionPathPlugin);
-gsap.set("#box", {y:"-=710", transformOrigin:"top"});
+gsap.set("#box", {y:"-=710", x:"-=150", transformOrigin:"top"});
 
 const mainTL = gsap.timeline(); 
 
 function TL(){
     const tl =gsap.timeline();
-    tl.to("#box", {duration:5, y:"+=710", rotate:-40, ease:"none"}); 
+    tl.to("#box", {duration:3, y:"+=710", rotate:-40, ease:"none"}); 
     return tl;
 }
 
