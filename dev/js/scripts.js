96b5327422ee3a6f620fa72aceadb55334c1b990
yukiiimak <yukimak821@gmail.com>
2021-11-28 01:25:58 -0500

    made box tilt back to upright position

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -2,13 +2,14 @@ import { gsap } from "gsap";
 import { MotionPathPlugin } from "gsap/MotionPathPlugin";
 
 gsap.registerPlugin(MotionPathPlugin);
-gsap.set("#box", {y:"-=710", x:"-=150", transformOrigin:"top"});
+gsap.set("#box", {y:"-=710", transformOrigin:"top"});
 
 const mainTL = gsap.timeline(); 
 
 function TL(){
     const tl =gsap.timeline();
-    tl.to("#box", {duration:3, y:"+=710", rotate:-40, ease:"none"}); 
+    tl.to("#box", {duration:2, y:"+=710", rotate:-40, ease:"none"}); 
+    tl.to("#box", {duration:1, rotate:0, ease:"none"});
     return tl;
 }
 
