fe360f69ced9c787753bacdd72137d48618c2303
yukiiimak <yukimak821@gmail.com>
2021-11-29 01:13:23 -0500

    tried to turn piping bag to its side

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -8,11 +8,11 @@ const mainTL = gsap.timeline();
 
 function TL(){
     const tl =gsap.timeline();
-    tl.to("#box", {duration:2, y:"+=710", rotate:-40, ease:"none"}, "fall-tilt"); 
-    tl.to("#box", {duration:1.5, rotate:0, ease:"none"}, "upright");
-    tl.to("#cover", {duration:2, rotate:-35, delay:.3, ease:"none"}, "fall-tilt");
-    tl.to("#cover", {duration:1.5, rotate:0, ease:"none"}, "upright"); 
-    // tl.to("#piping-bag", {duration:15, motionPath:{path:"#motion-path", align:"#motion-path"}}); 
+    // tl.to("#box", {duration:2, y:"+=710", rotate:-40, ease:"none"}, "fall-tilt"); 
+    // tl.to("#box", {duration:1.5, rotate:0, ease:"none"}, "upright");
+    // tl.to("#cover", {duration:2, rotate:-35, delay:.3, ease:"none"}, "fall-tilt");
+    // tl.to("#cover", {duration:1.5, rotate:0, ease:"none"}, "upright"); 
+    tl.to("#piping-bag", {duration:20, motionPath:{path:"#motion-path", align:"#motion-path", autoRotate: true, alignOrigin:[0, 0.1]}, ease: "sine.inOut"}); 
     return tl;
 }
 
