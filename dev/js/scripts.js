ec001f4b17d3133ab0c53faac3392178902f0947
yukiiimak <yukimak821@gmail.com>
2021-12-05 15:41:31 -0500

    got piping bag to move in direction i wanted

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -8,19 +8,20 @@ const mainTL = gsap.timeline();
 
 function TL(){
     const tl =gsap.timeline();
-    tl.to("#box", {duration:2, y:"+=710", rotate:-40, ease:"none"}, "fall-tilt"); 
-    tl.to("#box", {duration:1.5, rotate:0, ease:"none"}, "upright");
-    tl.to("#cover", {duration:2, rotate:-35, delay:.3, ease:"none"}, "fall-tilt");
-    tl.to("#cover", {duration:1.5, rotate:0, ease:"none"}, "upright"); 
-    // tl.to("#piping-bag", {duration:20, motionPath:{path:"#motion-path", align:"#motion-path", autoRotate: true, alignOrigin:[0, 0.1]}, ease: "sine.inOut"}); 
+    // tl.to("#box", {duration:2, y:"+=710", rotate:-40, ease:"none"}, "fall-tilt"); 
+    // tl.to("#box", {duration:1.5, rotate:0, ease:"none"}, "upright");
+    // tl.to("#cover", {duration:2, rotate:-35, delay:.3, ease:"none"}, "fall-tilt");
+    // tl.to("#cover", {duration:1.5, rotate:0, ease:"none"}, "upright"); 
+    tl.to("#piping-bag", {duration:20, motionPath:{path:"#motion-path", align:"#motion-path", autoRotate: -90}, ease: "sine.inOut"}); 
     return tl;
 }
 
 // function pipingbag(){
 //     const tl =gsap.timeline();
 //     tl.to("#piping-bag", {duration:20, motionPath:{path:"#motion-path", align:"#motion-path"}, rotate:30});
-
 // }
+
+// alignOrigin:[0, 0.1]
 
 // ask on greensock forum to see if there is a code for rotating the piping bag how i want it 
 // second option is to create a function of just the piping bag and manually rotating it when i want it to
