2eb64c28bb65ced73d93654659531b46d4372c71
yukiiimak <yukimak821@gmail.com>
2021-12-06 02:22:08 -0500

    increased speed of thin frostings

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -16,10 +16,10 @@ function TL(){
     tl.to("#cover", {duration:2, rotate:-60, delay:.3, ease:"none"}, "fall-tilt");
     tl.to("#cover", {duration:3, rotate:0, ease:"none"}, "upright"); 
     tl.to("#piping-bag", {duration:17, delay:1.3, motionPath:{path:"#motion-path", align:"#motion-path", alignOrigin:[0.5, 0.5], autoRotate:-100}, ease: "sine.inOut"}, "fall-tilt");
-    tl.to("#thin-frosting-1", {duration:3, delay:4, motionPath:{path:"#thin-frosting-motion-path-1", align:"#thin-frosting-motion-path-1", alignOrigin:[0.5, 0], autoRotate:-80}, ease:"none"}, "fall-tilt"); 
-    tl.to("#thin-frosting-2", {duration:3, delay:4.6, motionPath:{path:"#thin-frosting-motion-path-2", align:"#thin-frosting-motion-path-2", alignOrigin:[0.5, 0], autoRotate:-80}, ease:"none"}, "fall-tilt"); 
-    tl.to("#thin-frosting-3", {duration:3, delay:6.7, motionPath:{path:"#thin-frosting-motion-path-3", align:"#thin-frosting-motion-path-3", alignOrigin:[0.5, 0.5], autoRotate:80}, ease:"none"}, "fall-tilt");
-    tl.to("#thin-frosting-4", {duration:3, delay:6.9, motionPath:{path:"#thin-frosting-motion-path-4", align:"#thin-frosting-motion-path-4", alignOrigin:[0.5, 0.6], autoRotate:80}, ease:"none"}, "fall-tilt");  
+    tl.to("#thin-frosting-1", {duration:2, delay:4, motionPath:{path:"#thin-frosting-motion-path-1", align:"#thin-frosting-motion-path-1", alignOrigin:[0.5, 0], autoRotate:-80}, ease:"none"}, "fall-tilt"); 
+    tl.to("#thin-frosting-2", {duration:2, delay:4.6, motionPath:{path:"#thin-frosting-motion-path-2", align:"#thin-frosting-motion-path-2", alignOrigin:[0.5, 0], autoRotate:-80}, ease:"none"}, "fall-tilt"); 
+    tl.to("#thin-frosting-3", {duration:2, delay:6.7, motionPath:{path:"#thin-frosting-motion-path-3", align:"#thin-frosting-motion-path-3", alignOrigin:[0.5, 0.5], autoRotate:80}, ease:"none"}, "fall-tilt");
+    tl.to("#thin-frosting-4", {duration:2, delay:6.9, motionPath:{path:"#thin-frosting-motion-path-4", align:"#thin-frosting-motion-path-4", alignOrigin:[0.5, 0.6], autoRotate:80}, ease:"none"}, "fall-tilt");  
     return tl;
 }
 
