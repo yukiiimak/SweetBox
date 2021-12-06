3dfba2c1fe618bf8ad14f8a685a4cf69dfaee5e2
yukiiimak <yukimak821@gmail.com>
2021-12-06 13:18:04 -0500

    registered plugins i needed

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -3,7 +3,7 @@ import { GSDevTools } from "gsap/GSDevTools";
 import { MotionPathPlugin } from "gsap/MotionPathPlugin";
 import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
 
-gsap.registerPlugin(MotionPathPlugin);
+gsap.registerPlugin(GSDevTools, MotionPathPlugin, MorphSVGPlugin);
 gsap.set("#box", {y:"-=710", transformOrigin:"top"});
 gsap.set("#piping-bag", {x:"-=400", y:"-=50", scale:0.8, transformOrigin:"center"});
 
@@ -18,7 +18,7 @@ function TL(){
     tl.to("#piping-bag", {duration:17, delay:1.3, motionPath:{path:"#motion-path", align:"#motion-path", alignOrigin:[0.5, 0.5], autoRotate:-100}, ease: "sine.inOut"}, "fall-tilt");
     tl.from("#frosting-1", {duration:1.5, delay:6.8, autoAlpha:0}, "upright");
     tl.from("#frosting-2", {duration:1.7, delay:7.7, autoAlpha:0}, "upright");
-    tl.from("#frosting-3", {duration:2.3, delay:8.9, autoAlpha:0}, "upright");
+    tl.from("#frosting-3", {duration:2.4, delay:8.9, autoAlpha:0}, "upright");
     tl.from("#frosting-4", {duration:1.4, delay:9.8, autoAlpha:0}, "upright");
     tl.from("#box-word", {duration:1, delay:13, autoAlpha:0}, "upright");
     tl.from("#sweet-word", {duration:1, delay:13.5, autoAlpha:0}, "upright");
@@ -31,7 +31,7 @@ function thinfrostings(){
     tl.to("#thin-frosting-1", {duration:2, delay:3.9, motionPath:{path:"#thin-frosting-motion-path-1", align:"#thin-frosting-motion-path-1", alignOrigin:[0.5, 0], autoRotate:-80}, ease:"none"}, "fall-tilt"); 
     tl.to("#thin-frosting-2", {duration:2, delay:4.6, motionPath:{path:"#thin-frosting-motion-path-2", align:"#thin-frosting-motion-path-2", alignOrigin:[0.5, 0], autoRotate:-80}, ease:"none"}, "fall-tilt"); 
     tl.to("#thin-frosting-3", {duration:2, delay:6.5, motionPath:{path:"#thin-frosting-motion-path-3", align:"#thin-frosting-motion-path-3", alignOrigin:[0.5, 0.5], autoRotate:80}, ease:"none"}, "fall-tilt");
-    tl.to("#thin-frosting-4", {duration:2, delay:6.8, motionPath:{path:"#thin-frosting-motion-path-4", align:"#thin-frosting-motion-path-4", alignOrigin:[0.5, 0.6], autoRotate:80}, ease:"none"}, "fall-tilt");  
+    tl.to("#thin-frosting-4", {duration:2, delay:6.8, motionPath:{path:"#thin-frosting-motion-path-4", align:"#thin-frosting-motion-path-4", alignOrigin:[0.5, 0.5], autoRotate:80}, ease:"none"}, "fall-tilt");  
     tl.to("#thin-frosting-1", {duration:3, delay:9, motionPath:{path:"#thin-frosting-motion-path-1-2", align:"#thin-frosting-motion-path-1-2", alignOrigin:[0.5, 0.5], autoRotate:-100}, ease:"none"}, "fall-tilt");
     tl.to("#thin-frosting-2", {duration:3, delay:9, motionPath:{path:"#thin-frosting-motion-path-2-2", align:"#thin-frosting-motion-path-2-2", alignOrigin:[0.5, 0.5], autoRotate:-100}, ease:"none"}, "fall-tilt"); 
     tl.to("#thin-frosting-3", {duration:3, delay:9, motionPath:{path:"#thin-frosting-motion-path-3-2", align:"#thin-frosting-motion-path-3-2", alignOrigin:[0.5, 0.5], autoRotate:100}, ease:"none"}, "fall-tilt");
